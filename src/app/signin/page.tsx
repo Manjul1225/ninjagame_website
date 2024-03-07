3eefaf59e1d9e848a83cdf2050ba2c40b3800dd7
Manjul1225 <manjulbhargava660@gmail.com>
2024-03-07 23:32:33 +0900

    fix full name place holder

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -133,7 +133,7 @@ const SigninPage = () => {
                     <input
                       type="text"
                       name="name"
-                      placeholder="Enter your full name"
+                      placeholder="Enter your User name"
                       className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                       value={username}
                       onChange={e => setusername(e.target.value)}
