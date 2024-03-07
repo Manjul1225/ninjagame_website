013a5780757c8c426b90f854dc0072539f85f3f9
Manjul1225 <manjulbhargava660@gmail.com>
2024-03-07 23:31:16 +0900

    place holder fix

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -128,7 +128,7 @@ const SignupPage = () => {
                     <input
                       type="text"
                       name="name"
-                      placeholder="Enter your full name"
+                      placeholder="Enter your User name"
                       className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
                       value={fullname}
                       onChange={e => setFullName(e.target.value)}
