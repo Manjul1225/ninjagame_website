386a9a90ff8023bc67eda496c5866893ceeb8ba7
manjul1225 <manjulbhargava660@gmail.com>
2024-04-08 20:20:43 +1000

    error_fix

--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -1,13 +1,30 @@
-// import { NextResponse, NextRequest } from "next/server"
+import { NextResponse, NextRequest } from "next/server"
 
-// import {connect, close} from "@/libs/mongodb"
-// import Logs from "@/models/Logs"
+import {connect, close} from "@/libs/mongodb"
+import Logs from "@/models/Logs"
 
-// export async function POST(request: NextRequest) {
-//   const { account, cpoint, bpoint, apoint, admin, img } = await request.json()
-//   await connect()
-//   await Logs.create({ account, cpoint, bpoint, apoint, admin, img })
-//   close();
+export async function POST(request: NextRequest) {
+  const { account, cpoint, bpoint, apoint, admin, img } = await request.json()
+  await connect()
+  await Logs.create({ account, cpoint, bpoint, apoint, admin, img })
+  close();
+  return NextResponse.json(
+    { message: "The data has been added to the your database collection" },
+    { status: 201 }
+  )
+}
+
+export async function GET() {
+  await connect()
+  const logs = await Logs.find()
+  close();
+  return NextResponse.json({ logs })
+}
+
+// export async function DELETE(request) {
+//   const id = request.nextUrl.searchParams.get("id")
+//   await ConnectDB()
+//   await Logs.findByIdAndDelete(id)
 //   return NextResponse.json(
 //     { message: "The data has been added to the your database collection" },
 //     { status: 201 }
@@ -15,24 +32,7 @@
 // }
 
 // export async function GET() {
-//   await connect()
+//   await ConnectDB()
 //   const logs = await Logs.find()
-//   close();
 //   return NextResponse.json({ logs })
 // }
-
-// // export async function DELETE(request) {
-// //   const id = request.nextUrl.searchParams.get("id")
-// //   await ConnectDB()
-// //   await Logs.findByIdAndDelete(id)
-// //   return NextResponse.json(
-// //     { message: "The data has been added to the your database collection" },
-// //     { status: 201 }
-// //   )
-// // }
-
-// // export async function GET() {
-// //   await ConnectDB()
-// //   const logs = await Logs.find()
-// //   return NextResponse.json({ logs })
-// // }
